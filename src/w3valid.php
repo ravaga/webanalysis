017f83959c5519a804bc6ab19cd9d75af56046e2
Ale <ale@Ales-MacBook-Pro.local>
2016-07-26 00:55:13 -0700

    this

--- src/w3valid.php
+++ src/w3valid.php
@@ -5,10 +5,12 @@
            $get = urlencode($_GET["url"]);
            
         
+        
+        
+    
         $url = "https://validator.w3.org/check?uri=".$get."&output=json";
         
-        $url2 = "https://validator.w3.org/nu/?doc=http://www.google.com&out=json";
-        
+        $url2 = "https://validator.w3.org/nu/?showsource=false&doc=http://www.google.com&out=json";
         
          // Initializing curl
         $curl = curl_init( $url );
@@ -17,8 +19,14 @@
         $options = array(
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_HTTPHEADER => array('Content-type: application/json') ,
+            CURLOPT_HTTPHEADER, array('Accept: application/json'),
+            
         );
 
+        //curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
+
+        
+        
         // Setting curl options
         curl_setopt_array( $curl, $options );
 
@@ -26,18 +34,23 @@
         $result =  curl_exec($curl); // Getting jSON result string
         curl_close($curl);
         
-        //$new = htmlentities($result);
-        $string = substr($result , strpos($result , "{"));
+        //show html entities
+        $result_html = htmlentities($result);
+        //remove backslashes
+        $result_noSlashes = stripcslashes(stripcslashes($result_html));
+        //remove triple spaces
+        $noTriple = str_replace("   ", " ", $result_noSlashes);
         
+        //no headers
+        $noHeaders = substr($noTriple , strpos($noTriple , "{"));
         
-        $replace = json_encode(str_replace('"\"', " ", $string));
+        //$json = preg_replace('/(\\\n)+/m', '\\\n', $result_html);
+
         
-        $htmlfreecontent = strip_tags($replace);
-        
-        $json = json_encode($htmlfreecontent);
-            
-       print_r($json);     
-            
+            echo("<pre>");
+       
+            print_r($noHeaders);     
+            echo("</pre>");
             
        }
 ?>
