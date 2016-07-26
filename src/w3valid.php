cad5e23ad38d1ec3783b756bb3809cb681230bad
Rafa's iMac <rafackyou@gmail.com>
2016-07-25 22:17:02 -0700

    still

--- src/w3valid.php
+++ src/w3valid.php
@@ -7,10 +7,11 @@
         
         $url = "https://validator.w3.org/check?uri=".$get."&output=json";
         
+        $url2 = "https://validator.w3.org/nu/?doc=http://www.google.com&out=json";
         
         
          // Initializing curl
-        $ch = curl_init( $url );
+        $curl = curl_init( $url );
 
         // Configuring curl options
         $options = array(
@@ -18,14 +19,24 @@
             CURLOPT_HTTPHEADER => array('Content-type: application/json') ,
         );
 
-    // Setting curl options
-    curl_setopt_array( $ch, $options );
+        // Setting curl options
+        curl_setopt_array( $curl, $options );
 
-    // Getting results
-    $result =  curl_exec($ch); // Getting jSON result string
-      
+        // Getting results
+        $result =  curl_exec($curl); // Getting jSON result string
+        curl_close($curl);
+        
+        //$new = htmlentities($result);
+        $string = substr($result , strpos($result , "{"));
+        
+        
+        $replace = json_encode(str_replace('"\"', " ", $string));
+        
+        $htmlfreecontent = strip_tags($replace);
+        
+        $json = json_encode($htmlfreecontent);
             
-       var_dump($result);     
+       print_r($json);     
             
             
        }
